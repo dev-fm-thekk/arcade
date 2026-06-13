c7d0f808ebf129840dfb1d3812d3c7fce0c73eb7
Abhiram A R <abhiramajithr@gmail.com>
2026-06-13 11:18:03 +0530

    bg-music added, mobile optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { Routes, Route } from "react-router-dom"
-import ArcadeLayout from "./layouts/ArcadeLayout"
-import GameLayout from "./layouts/GameLayout"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Projects from "./pages/Projects"
-import Explore from "./pages/Explore"
-import BlankPage from "./pages/Blank"
+import { Routes, Route } from "react-router-dom";
+import React, { useRef, useState } from 'react';
+import ArcadeLayout from "./layouts/ArcadeLayout";
+import GameLayout from "./layouts/GameLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Explore from "./pages/Explore";
+import BlankPage from "./pages/Blank";
 
 function App() {
+
   return (
-     <Routes>
-      <Route path="/" element={<ArcadeLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="explore" element={<Explore />} />
-        <Route path="game/:id" element={<GameLayout />} />
-      </Route>
-      <Route path='/blank' element={<BlankPage />} />
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<ArcadeLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="game/:id" element={<GameLayout />} />
+        </Route>
+        <Route path="/blank" element={<BlankPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Blank.jsx
+++ src/pages/Blank.jsx
@@ -1,9 +1,8 @@
-import Home from "../components/arcade";
 
 export default function BlankPage() {
     return (
         <div>
-            <App />
+            
         </div>
     )
 }
